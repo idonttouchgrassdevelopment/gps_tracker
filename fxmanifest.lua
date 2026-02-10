6016317f7015897865d5d804b57770dfbf061edd
idonttouchgrass Development <158120388+idonttouchgrassdevelopment@users.noreply.github.com>
2026-02-10 11:25:24 -0500

    Updated fx manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ name 'idtg-tracker'
 
 author 'IDTG Development'
 description 'Advanced GPS Tracker System for FiveM Roleplay Servers'
-version '1.1.0'
+version '1.0.0'
 
 client_script 'client/client.lua'
 server_script 'server/server.lua'
@@ -13,7 +13,6 @@ server_script 'server/server.lua'
 shared_script 'config.lua'
 
 dependencies {
-    '/server:5181',
 }
 
 files {
